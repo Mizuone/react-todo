5bc0adcf3d1d86f2704342f5e3113e0caab0e5a3
mizueone <orlinskikyle@gmail.com>
2018-04-30 22:47:57 -0500

    styling task views

--- src/components/task-categories/views/focus-tasks-view.js
+++ src/components/task-categories/views/focus-tasks-view.js
@@ -9,7 +9,23 @@ export default class FocusTasksView extends Component {
 
     return (
       <div className={view}>
+        <form>
+          <div className="row">
+            <div className="col s3"></div>
+            <div className="input-field col s6 task-field-container">
+              <input id="focus-task" type="text" className="validate" />
+              <label for="focus-task">Task</label>
+            </div>
+            <div class="col s3">
+              <div className="btn-floating waves-effect waves-light close-task-menu">
+                <i className="material-icons">close</i>
+              </div>
+            </div>
+
+          </div>
+        </form>
       </div>
+
     )
   }
 
